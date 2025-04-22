f9d41e6a3d2fad9f0a02d1ad1bef14490b0d2e36
Tshewang Gyaltshen <tshewanggyaltshen0@gmail.com>
2025-04-22 16:24:17 +0600

    100% commitment

--- foundations/lib/main.dart
+++ foundations/lib/main.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:foundations/pages/first_page.dart';
+import 'package:foundations/start_screen.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: FirstPage());
-  }
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 132, 72, 142),
+                Color.fromARGB(255, 104, 18, 119),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
